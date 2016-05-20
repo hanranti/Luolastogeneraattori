a3f9716dfb2012dcc96ca660bc404e4281765681
hanranti <hannu.rantinoja@helsinki.fi>
2016-05-20 08:34:54 +0300

    args 0

--- Luolastogeneraattori/src/luolastogeneraattori/Luolastogeneraattori.java
+++ Luolastogeneraattori/src/luolastogeneraattori/Luolastogeneraattori.java
@@ -3,6 +3,11 @@ package luolastogeneraattori;
 public class Luolastogeneraattori {
 
     public static void main(String[] args) {
+        int size = 32;
+
+        if (args.length > 0) {
+            size = Integer.parseInt(args[0]);
+        }
     }
 
 }
