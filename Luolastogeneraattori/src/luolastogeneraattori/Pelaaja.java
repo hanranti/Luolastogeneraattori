e10fb7eb6db437d7f8465758d1f06f5f361a7356
hanranti <hannu.rantinoja@helsinki.fi>
2016-05-20 08:36:18 +0300

    trsd

---
+++ Luolastogeneraattori/src/luolastogeneraattori/Pelaaja.java
@@ -0,0 +1,14 @@
+package luolastogeneraattori;
+
+public class Pelaaja {
+
+    private int x, y, luolaX, luolaY;
+
+    public Pelaaja(int x, int y, int luolaX, int luolaY) {
+        this.x = x;
+        this.y = y;
+        this.luolaX = luolaX;
+        this.luolaY = luolaY;
+    }
+
+}
