25fba4b06be986f32031078f4212b84b72debd9e
Hannu Rantinoja <hannu.rantinoja@cs.helsinki.fi>
2016-05-30 14:01:53 +0300

    vanha poistettu

--- LuolastoGeneraattori/src/luolasto/Luolasto.java
+++ LuolastoGeneraattori/src/luolasto/Luolasto.java
@@ -53,16 +53,20 @@ public class Luolasto {
      * mukaisilla koordinaateilla palautetaan oikea luola.
      */
     private void kasvataTaulukkoa() {
-        int vanhaMuutos = muutos;
-        muutos = luolasto.length / 2;
+        int uusiMuutos = luolasto.length / 2;
         Luola[][] luolasto2 = new Luola[luolasto.length * 2][luolasto[0].length * 2];
         boolean[][] generoitu2 = new boolean[luolasto2.length][luolasto2[0].length];
+        System.out.println("uusimuutos " + uusiMuutos);
         for (int i = 0; i < luolasto.length; i++) {
             for (int j = 0; j < luolasto[0].length; j++) {
-                luolasto2[muutos + i][muutos + j] = luolasto[vanhaMuutos + i][vanhaMuutos + j];
-                generoitu2[muutos + i][muutos + j] = generoitu[vanhaMuutos + i][vanhaMuutos + j];
+                System.out.println("i" + i + "j" + j);
+                luolasto2[uusiMuutos - muutos+ i][uusiMuutos -muutos + j] 
+                        = luolasto[i][ j];
+                generoitu2[uusiMuutos -muutos+ i][uusiMuutos-muutos+ j] 
+                        = generoitu[ i][j];
             }
         }
+        muutos = uusiMuutos;
         luolasto = luolasto2;
         generoitu = generoitu2;
     }
